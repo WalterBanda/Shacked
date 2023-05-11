e0b4af3c76df7d8650b647f2bf23f8424ea944c7
Walter Banda <walterkaimurima@gmail.com>
2023-05-12 00:10:31 +0300

    🔧 Linting update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import typescript from 'vite-plugin-typescript';
+import typescript from 'vite-plugin-typescript'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),typescript()],
+  plugins: [react(), typescript()],
   server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
-});
+    host: '0.0.0.0',
+    port: 3000
+  }
+})
