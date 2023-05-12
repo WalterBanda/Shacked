054a6410e92f6db7bb3b23c3f09b29d9adf8cf2f
Walter Banda <walterkaimurima@gmail.com>
2023-05-12 21:34:54 +0300

    ☕ Added avatar into the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,14 @@
+import { Avatar, AvatarFallback } from '@/ui/avatar'
+
 export default function Navbar() {
   return (
-    <div className='flex flex-row items-center'>
+    <div className='flex flex-row items-center justify-between'>
       <h1>Golem Sandbox</h1>
-      <div>Account details</div>
+      <div>
+        <Avatar>
+          <AvatarFallback></AvatarFallback>
+        </Avatar>
+      </div>
     </div>
   )
 }
